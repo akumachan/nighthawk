9e967cc4a3ef549426c4021a97d1bb51fd21e0b2
akumachan <akumachanit@gmail.com>
2020-01-30 00:37:23 +0900

    Fix: Warning in developer console.
    Put dummy strings in href.

--- src/common/Header.js
+++ src/common/Header.js
@@ -4,9 +4,9 @@ export default class Header extends React.Component{
   render() {
     return (
       <header>
-        <a href=""><i className="material-icons menu">menu</i></a>
+        <a href="/#"><i className="material-icons menu">menu</i></a>
         <span className="service_name">Nighthawk</span>
-        <a href=""><i className="material-icons save">save</i></a>
+        <a href="/#"><i className="material-icons save">save</i></a>
       </header>
     );
   }
